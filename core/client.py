14ec85e7ee3232cb7928c2de04748d68ae15eb96
Samo Karapetyan <samvel.karapetyan.1994@gmail.com>
2026-05-06 00:54:30 +0400

    YC-ի ավելացում, + get_by_category

---
+++ core/client.py
@@ -0,0 +1,32 @@
+import requests
+from requests.exceptions import RequestException, Timeout
+from models import API_CATEGORY, API_ITEM
+
+
+class YC:
+    def __init__(self):
+        self.headers = {}
+        self.url_category = ''
+
+    def get_by_category(self, category_id):
+        data_body = {
+            "categoryId": category_id,
+            "count": 10000,
+            "page": 1,
+            "parentId": category_id,
+        }
+
+        try:
+            res = requests.post(self.url_category, headers=self.headers, json=data_body, timeout=(5, 15))
+            res.raise_for_status()
+
+            try:
+                return API_CATEGORY.model_validate(res.json())
+            except Exception as e:
+                print(f"Տվյալների վալիդացման սխալ: {e}")
+        except Timeout:
+            print("Հարցումը չեղարկվեց ժամանակի սպառման պատճառով (Timeout).")
+        except RequestException as e:
+            print(f"Տեղի է ունեցել սխալ հարցման ընթացքում: {e}")
+
+        return None
